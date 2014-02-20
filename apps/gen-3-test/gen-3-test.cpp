9c9b7f0bd0db0704596e096ae605087a0764a452
Michael Bond <codemonkey85@gmail.com>
2014-02-20 09:21:18 -0500

    Update gen-3-test.cpp

--- apps/gen-3-test/gen-3-test.cpp
+++ apps/gen-3-test/gen-3-test.cpp
@@ -1,20 +1,15 @@
 #ifdef PKMDS_CMAKE_USED
-//#include <pkmds/pkmds_sql.h>
 #include <pkmds/pkmds_gba.h>
 #else
 //#include "..\\..\\include\\pkmds\\pkmds_sql.h"
 #include "..\\..\\include\\pkmds\\pkmds_gba.h"
 #endif
-//#include <string>
-//#include <locale>
 using namespace std;
-string outputpath = "C:\\Users\\Michael Bond\\Downloads\\GBA PKM DUMPED\\";
 int main(int argc, char* argv[])
 {
-	string savefile;
-	//savefile = "C:\\Users\\Michael Bond\\Dropbox\\Saves\\pokemon_emerald_version - Copy2.sav";
-	savefile = "C:\\Users\\Michael Bond\\Dropbox\\Saves\\Pokemon - Fire Red GBA_MODIFIED.sav";
-	const char * pkmfile = "C:\\Users\\Michael Bond\\Dropbox\\Saves\\Gengar_heart.3gpkm";
+	if(argc > 1)
+	{
+	string savefile = argv[1];
 	gbasavefilepacked* savin = new gbasavefilepacked();
 	gbasavefile* sav = new gbasavefile();
 	pokemon_gen3* gbapkm = new pokemon_gen3();
@@ -22,10 +17,6 @@ int main(int argc, char* argv[])
 	read(savefile.c_str(),savin);
 	buildgbasave(savin,sav);
 	opendb("C:\\Users\\Michael Bond\\Dropbox\\PKMDS Databases\\veekun-pokedex.sqlite");
-
-	//read(pkmfile,gbapkm);
-	//dostuff(gbapkm);
-
 	for(int box = 0; box < 14; box++)
 	{
 		for(int slot = 0; slot < 30; slot++)
@@ -35,24 +26,16 @@ int main(int argc, char* argv[])
 			shufflegbapkm(gbapkm,true);
 			if(gbapkm->data.species != GBASpecies::NOTHING)
 			{
-				//if(gbapkm->data.species == GBASpecies::squirtle)
-				//{
-				//uint32 * ribbons = reinterpret_cast<uint32*>(&(gbapkm->data.ribbons));
-				//if(*ribbons != 0)
-				//{
 				if((box == 0) & (slot == 0))
 				{
-					//dostuff(gbapkm);
 					convertgen3pkmtogen5(gbapkm,pkm);
+					cout << lookuppkmname(pkm) << "\n";
 				}
-				//}
-				//}
 			}
 		}
 	}
 	closedb();
 	cout << "\nEND\n";
-	string test;
-	//cin >> test;
+	}
 	return 0;
 }
