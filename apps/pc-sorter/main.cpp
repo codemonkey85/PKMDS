9726632f2a930082678ab345530d7b9099e201da
Michael Bond <codemonkey85@gmail.com>
2014-07-05 11:35:43 -0400

    Added some error handling

--- apps/pc-sorter/main.cpp
+++ apps/pc-sorter/main.cpp
@@ -8,43 +8,57 @@
 using namespace std;
 int main(int argc, char* argv[])
 {
-	string saveout = "OUT.sav";
-	bw2sav_obj* sav = new bw2sav_obj;
-	read(argv[1],sav);
-	bool bw2 = savisbw2(sav);
-	std::vector<pokemon_obj> *vpkm = new std::vector<pokemon_obj>;
-	vpkm = getpcvector(&(sav->cur));
-	std::vector<pokemon_obj>::const_iterator pkmit;
-	pkmit = vpkm->begin();
-	sortbyspeciesasc(*vpkm);
-	pokemon_obj * blankpkm = new pokemon_obj;
-	pkmit = vpkm->begin();
-	pokemon_obj * pkm = new pokemon_obj;
-	for(int box = 0; box < 24; box++)
+	if (argv[1] != NULL)
 	{
-		for(int slot = 0; slot < 30; slot++)
+		try
 		{
-			if(((box*30)+slot) < vpkm->size())
+			string saveout = "OUT.sav";
+			bw2sav_obj* sav = new bw2sav_obj;
+			read(argv[1], sav);
+			bool bw2 = savisbw2(sav);
+			std::vector<pokemon_obj> *vpkm = new std::vector<pokemon_obj>;
+			vpkm = getpcvector(&(sav->cur));
+			std::vector<pokemon_obj>::const_iterator pkmit;
+			pkmit = vpkm->begin();
+			sortbyspeciesasc(*vpkm);
+			pokemon_obj * blankpkm = new pokemon_obj;
+			pkmit = vpkm->begin();
+			pokemon_obj * pkm = new pokemon_obj;
+			for (int box = 0; box < 24; box++)
 			{
-				*pkm = *pkmit;
-				encryptpkm(pkm);
-				sav->cur.boxes[box].pokemon[slot] = *pkm;
-				pkmit++;
+				for (int slot = 0; slot < 30; slot++)
+				{
+					if (((box * 30) + slot) < vpkm->size())
+					{
+						*pkm = *pkmit;
+						encryptpkm(pkm);
+						sav->cur.boxes[box].pokemon[slot] = *pkm;
+						pkmit++;
+					}
+					else
+					{
+						sav->cur.boxes[box].pokemon[slot] = *blankpkm;
+					}
+				}
+				calcboxchecksum(&(sav->cur), box, bw2);
 			}
-			else
-			{
-				sav->cur.boxes[box].pokemon[slot] = *blankpkm;
-			}
+			fixsavchecksum(sav, bw2);
+			write(saveout.c_str(), sav);
+			delete pkm;
+			pkm = 0;
+			delete vpkm;
+			vpkm = 0;
+			delete sav;
+			sav = 0;
 		}
-		calcboxchecksum(&(sav->cur),box,bw2);
+		catch (char* strg)
+		{
+			cout << "Exception raised: " << strg << endl;
+		}
 	}
-	fixsavchecksum(sav,bw2);
-	write(saveout.c_str(),sav);
-	delete pkm;
-	pkm = 0;
-	delete vpkm;
-	vpkm = 0;
-	delete sav;
-	sav = 0;
+	else
+	{
+		cout << "No argument supplied for save file!\n";
+	}
 	return 0;
 }
